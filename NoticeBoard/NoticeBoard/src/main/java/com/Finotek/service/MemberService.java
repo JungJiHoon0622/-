e58b98c504bf3e9143a1a41cb54edcf84234d628
JungJiHoon0622 <107619923+JungJiHoon0622@users.noreply.github.com>
2022-08-17 19:26:49 +0900

    0817

---
+++ NoticeBoard/NoticeBoard/src/main/java/com/Finotek/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.Finotek.service;
+
+import com.Finotek.vo.MemberVO;
+
+public interface MemberService {
+
+	public void register(MemberVO vo) throws Exception;
+	
+	public MemberVO login(MemberVO vo) throws Exception;
+}
